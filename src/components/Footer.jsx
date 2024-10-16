110971607ad7c23910fa5f7d3e9a696b5eda706c
GCespedes <giovannice22@hotmail.com>
2024-10-16 01:28:45 -0500

    home advance

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
+import { Link } from "react-router-dom";
 import "./footer.css";
+import { useTranslation } from "react-i18next";
 
 function Footer() {
-  return <div className="footer">Footer</div>;
+  const { t } = useTranslation();
+  return (
+    <div className="footer">
+      <ul>
+        {" "}
+        <li>
+          <Link to="/">{t("navbar.home")}</Link>
+        </li>
+        <li>
+          <Link to="/about">{t("navbar.about")}</Link>
+        </li>
+        <li>
+          <Link to="/academy">{t("navbar.academy")}</Link>
+        </li>
+        <li>
+          <Link to="/club">{t("navbar.club")}</Link>
+        </li>
+        <li>
+          <Link to="/conetworking">{t("navbar.conetworking")}</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import "./home.css";
 import background from "../assets/Home/galaxyEarthMilkyWay.jpg";
 import alienbook from "../assets/Home/alienbook.webp";
+import academy from "../assets/Home/Academy.webp";
+import club from "../assets/Home/Club.webp";
+import conetwork from "../assets/Home/Conetwork.webp";
 
 function Landing() {
   return (
@@ -19,7 +22,7 @@ function Landing() {
         <button className="normalbutton">Start Now!</button>
         <img src={alienbook} className="image1" />
       </section>
-      <section>
+      <section className="second-home">
         <div className="buttonlist">
           <button className="normalbutton">DeFi</button>
           <button className="normalbutton">DeSci</button>
@@ -28,8 +31,105 @@ function Landing() {
         </div>
         <div></div>
       </section>
-      <section></section>
-      <section></section>
+      <section className="third-home">
+        <div>
+          <button className="specialbutton">
+            <img src={academy} />
+          </button>
+          Learn & Win:
+        </div>
+        <div>
+          <button className="specialbutton">
+            <img src={club} />
+          </button>
+          Enjoy Advantage:
+        </div>
+        <div>
+          <button className="specialbutton">
+            <img src={conetwork} />
+          </button>
+          Elevate your succes:
+        </div>
+      </section>
+      <section className="fourth-home">
+        <button className="normalbutton">Academy</button>
+        <button className="normalbutton">Clubs</button>
+        <button className="normalbutton">CoNetWorking</button>
+      </section>
+      <section className="fifth-home">
+        <h1>Financial Freedom</h1>
+        <p>
+          Any Individual or Professional can understand the ADVANTAGES of
+          digital money.
+          <br />
+          Impartial money that does not discriminate, decentralized currencies;
+          stable without volatility, as well as safe reserves of values.
+          <br />
+          Access with Crypto, NFTs, Tokens to Order & Generate all types of
+          Experiences, Products & Services according to your demands, instantly
+        </p>
+      </section>
+      <section className="sixth-home">
+        <div>
+          <img></img>
+          Cash Money Peer To Peer (P2P)
+        </div>
+        <div>
+          <img></img>
+          Person to Person Digital Money
+        </div>
+        <div>
+          <img></img>
+          Wallet Connect Web 3!
+        </div>
+      </section>
+      <section className="seventh-home">
+        <h1>FAQS Questions?</h1>
+        <p>
+          Whether you're curious about features, a free trial, or even press
+          releases, we're here to answer all your questions.
+        </p>
+        <button className="normalbutton">Contact Now!</button>
+        <img src="" />
+      </section>
+      <section className="eighth-home">
+        <article>
+          <div className="icon" />
+          <div>
+            <h3>SECURE PAYMENT</h3>
+            <p>All our payments are encrypted and secured with SSL</p>
+          </div>
+        </article>
+        <article>
+          <div className="icon" />
+          <div>
+            <h3>DELIVERY WITH CARE</h3>
+            <p>
+              Ultra-fast shipping to your door with efficiency & sustainability
+            </p>
+          </div>
+        </article>
+        <article>
+          <div className="icon" />
+          <div>
+            <h3>EXCELLENT SERVICE</h3>
+            <p>Live chat and 24/7 support guarantee it</p>
+          </div>
+        </article>
+      </section>
+      <section className="nineth-home">
+        <h3>Earth Powered Space</h3>
+        <ul>
+          <li>mail</li>
+          <li>face</li>
+          <li>insta</li>
+          <li>linkedin</li>
+          <li>telegram</li>
+          <li>twitter</li>
+          <li>whats</li>
+          <li>youtu</li>
+        </ul>
+      </section>
       <img className="background" src={background} />
     </div>
   );
