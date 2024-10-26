c0ce76ca79d8bd69e4dd48b0c693b2b24e0f20d1
GCespedes <giovannice22@hotmail.com>
2024-10-26 01:43:56 -0500

    Academy advance

--- src/pages/Academy.jsx
+++ src/pages/Academy.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import "./academy.css";
+import background from "../assets/Home/galaxyEarthMilkyWay.jpg";
+import absolute from "../assets/Academy/Absolute.webp";
 
 function Academy() {
   return (
-    <div>Academy</div>
-  )
+    <div className="academy">
+      <section className="first-academy">
+        <h1>Academia</h1>
+        <h3>¡Accede ahora para aprender y ganar!</h3>
+        <img src={absolute} />
+      </section>
+
+      <section className="second-academy">
+        <p>
+          Adquiere habilidades de mindfulness para descubrir conocimientos y
+          destrezas que te ayudarán a evolucionar y te impulsarán hacia una
+          experiencia óptima en plenitud de flujo; cómo mejorar los beneficios
+          en tu calidad de vida con ganancias sostenibles, obteniendo bienestar
+          emocional en plenitud, crecimiento y desarrollo a nivel personal y
+          profesional, explorando tanto técnicas ancestrales como tecnologías de
+          vanguardia…
+        </p>
+        <p>
+          Conéctate en un nivel más profundo contigo mismo, con tu mundo y con
+          este planeta. ¡Inicia tu transformación ahora!
+        </p>
+        <h4>Módulos de formación</h4>
+        <ul>
+          <li>Abundancia y Libertad (EcoFinTech, Comercio Electrónico)</li>
+          <li>Autogestión y Sostenibilidad (Ecología, Permacultura)</li>
+          <li>
+            Armonía y Trascendencia (Alquimia, Mindfulness enfocado,
+            Meditaciones)
+          </li>
+        </ul>
+        <h4>Bootcamps</h4>
+        <ul>
+          <li>Inteligencia Artificial (Red Neuronal)</li>
+          <li>Desarrollo Blockchain</li>
+          <li>Creación de Proyectos Web 3</li>
+          <li>Fundamentos de Programación</li>
+          <li>Computación Cuántica</li>
+        </ul>
+        <button className="normalbutton">Access Now!</button>
+      </section>
+
+      <section className="third-academy"></section>
+
+      <img className="background" src={background} />
+    </div>
+  );
 }
 
-export default Academy
+export default Academy;
