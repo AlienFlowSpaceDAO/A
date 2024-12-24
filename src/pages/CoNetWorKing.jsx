5fdb08f527c9b0e682d668cfa30f2e41821a452e
Giovanni Cespedes <giovannice22@hotmail.com>
2024-12-23 20:43:48 -0500

    actualizado

--- src/pages/CoNetWorKing.jsx
+++ src/pages/CoNetWorKing.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import background from "../assets/CoNetWorking/CoNetWorking.webp";
+import "./conetworking.css"
 
 function Conetworking() {
   return (
-    <div>Conetworking</div>
+    <div className="conetworking">
+    <section className="first-conetworking">
+      <h2>CoNetWorking</h2>
+      <p>Colaboramos para optimizar tus flujos de trabajo. Participa en el ecosistema DAO, lleno de oportunidades y posibilidades para formar alianzas valiosas con ganancias sostenibles. ¡Conéctate, prospera, triunfa!</p>
+      <button className="normalbutton">Contact Now</button>
+    </section>
+    <section className="second-conetworking">
+      <div>Hacemos que participar en nuestro Ecosistema DAO sea accesible, claro y directo, incluso para aquellos que están dando sus primeros pasos en este emocionante viaje. Si tienes preguntas o necesitas más información, estamos aquí para ayudarte. ¡Bienvenido a la Evolución de las Finanzas!</div>
+      <div>
+        <h2>AMM (Market Maker Automatizado)</h2>
+        <p>¡Compra y vende criptomonedas de forma automatizada y sencilla! Facilita los intercambios sin necesidad de intermediarios mediante contratos inteligentes automatizados que usan redes de inteligencia neuronal para comprar barato y vender caro.</p>
+      </div>
+      <div>
+        <h2>DeFi (Finanzas Descentralizadas)</h2>
+        <p>Explora un nuevo enfoque financiero. Accede a servicios financieros sin depender de instituciones tradicionales, con tus propias claves privadas y sin exponer datos sensibles.</p>
+      </div>
+      <div>
+        <h2>ReFi (Finanzas Descentralizadas y Regenerativas)</h2>
+        <p>¡Gana manteniéndote activo! Nuestro sistema de Staking te permite obtener recompensas al mantener tus criptomonedas con nuestro CoNetWorKing.</p>
+      </div>
+      <div>
+        <h2>Inversión Dual</h2>
+        <p>Maximiza tus ganancias participando en diferentes oportunidades de inversión al mismo tiempo.</p>
+      </div>
+      <div>
+        <h2>Farming</h2>
+        <p>Siembra y cosecha tus recompensas. Nuestro sistema de permacultura te permite ganar más criptomonedas al participar activamente en la red.</p>
+      </div>
+      <div>
+        <h2>Mining</h2>
+        <p>Mina criptomonedas de forma sencilla. Con nuestro sistema de minería, puedes contribuir a la red y recibir recompensas por ello.</p>
+      </div>
+    </section>
+    <section className="third-conetworking"></section>
+    <section className="fort-conetworking"></section>
+    <section className="fift-conetworking"></section>
+    <img className="background" src={background} />
+    </div>
   )
 }
 
